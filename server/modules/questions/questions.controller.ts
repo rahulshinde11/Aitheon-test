c093c602db2c701ccb17f85bfde3beddf43c7caa
Rahul <rshinde@aitheon.com>
2020-02-05 13:45:39 +0200

    moved questions to data folder

--- server/modules/questions/questions.controller.ts
+++ server/modules/questions/questions.controller.ts
@@ -2,13 +2,14 @@ import { Request, Response, response } from 'express';
 import { Question, questionSchema } from './question.model';
 import { environment } from '../../environment';
 import * as request from 'request-promise';
+import * as fs from 'fs';
+import path from 'path';
+import * as util from 'util';
+const reader = util.promisify(fs.readFile);
 
 const loadQuestions = async () => {
-    const url = environment.questionsApi;
-    const questions = await request.get(url, {json: true});
-
-    return questions;
-
+    const questions = await reader(path.resolve('./server/data/questions.json'), 'utf8');
+    return JSON.parse(questions);
 }
 
 export const getQuestions = async (req: Request, res: Response ) => {
