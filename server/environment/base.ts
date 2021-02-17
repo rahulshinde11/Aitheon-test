09e1937b15f1a5f6ebb09afb1f308632a1985f9d
Rahul <rshinde@aitheon.com>
2021-02-17 17:50:21 +0530

    removed connection string

--- server/environment/base.ts
+++ server/environment/base.ts
@@ -2,7 +2,7 @@ export const environment = {
   port: process.env.PORT || 3000,
   production: false,
   db: {
-    uri: process.env.MONGODB_URI || 'mongodb+srv://rahul:sspv9hncxLi0tGA7@cluster0-cgi2o.mongodb.net/test?retryWrites=true&w=majority'
+    uri: process.env.MONGODB_URI
   },
   questionsApi: 'https://opentdb.com/api.php?amount=15&type=multiple'
 
